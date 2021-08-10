d5c8271466410db10ea8aa40ac7528bf9f5e1bd5
ALEXANDER <msumba83@gmail.com>
2021-08-10 22:22:48 +0200

    done up to if else

---
+++ UDM-JAVASCRIPT/Random number generation/randomNumber.js
@@ -0,0 +1,14 @@
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+
+
+// console.log("The answer is: " + n);
+
+console.log("What is your name");
+console.log("What is their name");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.random(loveScore) + 1;
+
+console.log("Your Love Score is " + loveScore  + " %");
