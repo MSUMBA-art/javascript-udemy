2912533f9d6a93afa06afd32679e912bb4d17566
ALEXANDER <msumba83@gmail.com>
2021-08-10 22:24:40 +0200

    done index.js

---
+++ UDM-JAVASCRIPT/if-else-&logic/conditionals.js
@@ -0,0 +1,30 @@
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+
+// console.log("The answer is: " + n);
+
+console.log("What is your name");
+console.log("What is their name");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.random(loveScore) + 1;
+if (loveScore > 100) {
+    console.log("Your Love Score is " + loveScore + " %" + "You love him");
+} else {
+    console.log("Your Love Score is " + loveScore + " %");
+}
+
+// COMPARATORS
+
+/* === is equal to
+!== is not equal to
+> is greater than
+< is lesser than
+>= is greater or equal to
+<= is lesser or equal to
+&& and
+|| or
+! not
+*/
+
--- UDM-JAVASCRIPT/index.js
+++ UDM-JAVASCRIPT/index.js
@@ -1,3 +1,41 @@
-function {
-    
+// function getMilk(money) {
+//   console.log("leaveHouse");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   console.log("moveUp");
+//   console.log("moveUp");
+
+//   var numberOfBottles = money / 1.5;
+
+//   console.log("Please buy " + numberOfBottles  + " bottles of milk");
+//   console.log("moveUp");
+//   console.log("movRight");
+//   console.log("moveRight");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   console.log("moveDown");
+//   return money % 1.5;
+// }
+
+// var change = getMilk(4);
+
+// console.log(change)
+ 
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+function bmiCalculator(weight, height)  {
+    var bmi = weight / (Math.pow(height, 2));
+    return Math.round(bmi) ;
 }
+
+var bmi = bmiCalculator(65, 1.8); 
+console.log("The answer is:" + bmi);
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
