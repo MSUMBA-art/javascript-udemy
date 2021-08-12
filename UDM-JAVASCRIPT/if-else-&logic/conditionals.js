2a467b51e0428ed43487be713ee2c428f87723cb
ALEXANDER <msumba83@gmail.com>
2021-08-12 07:28:18 +0200

    done

--- UDM-JAVASCRIPT/if-else-&logic/conditionals.js
+++ UDM-JAVASCRIPT/if-else-&logic/conditionals.js
@@ -1,33 +1,54 @@
-// var n = Math.random();
-// n = n * 6;
-// n = Math.floor(n) + 1;
+// // var n = Math.random();
+// // n = n * 6;
+// // n = Math.floor(n) + 1;
 
-// console.log("The answer is: " + n);
+// // console.log("The answer is: " + n);
 
-console.log("What is your name");
-console.log("What is their name");
+// console.log("What is your name");
+// console.log("What is their name");
 
-var loveScore = Math.random() * 100;
-loveScore = Math.random(loveScore) + 1;
-if (loveScore > 100) {
+// var loveScore = Math.random() * 100;
+// loveScore = Math.random(loveScore) + 1;
+// if (loveScore > 100) {
 
-  if (loveScore > 30 && loveScore <= 70) {
-    console.log("Your Love Score is " + loveScore + " %" + "You love him");
+//   if (loveScore > 30 && loveScore <= 70) {
+//     console.log("Your Love Score is " + loveScore + " %" + "You love him");
+//   }
+
+//   if (loveScore => 30 ) {
+//     console.log("Your Love Score is " + loveScore + " %");
+//   }
+// } else {
+//     console.log("Your Love Score is " + loveScore + " %" + " yes");
+// }
+
+// // COMPARATORS
+
+// /* === is equal to
+// !== is not equal to
+// > is greater than
+// < is lesser than
+// >= is greater or equal to
+// <= is lesser or equal to
+// && and
+// || or
+// ! not
+// */
+
+function isLeap(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return "Leap year";
+      } else {
+        return "Not a leap year";
+      }
+    } else {
+      return "Leap year";
+    }
+  } else {
+    return "Not leap year";
   }
-} else {
-    console.log("Your Love Score is " + loveScore + " %");
 }
 
-// COMPARATORS
-
-/* === is equal to
-!== is not equal to
-> is greater than
-< is lesser than
->= is greater or equal to
-<= is lesser or equal to
-&& and
-|| or
-! not
-*/
-
+isLeap();
