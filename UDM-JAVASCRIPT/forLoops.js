63f358c353a85d2b4f2db7b71999bade5ff2eb7e
ALEXANDER <msumba83@gmail.com>
2021-08-22 14:32:53 +0200

    Update forLoops.js

---
+++ UDM-JAVASCRIPT/forLoops.js
@@ -0,0 +1,17 @@
+// for (let i = 1; i < 2; i++) {
+//     console.log(i);
+    
+// }
+
+function name(n) {
+    
+    let outPut = [];
+    if (n === 1) {
+        outPut = [0, 1]
+    }
+    else {
+        outPut =[0, 1];
+        outPut [0] + outPut[1];
+        outPut.push(outPut[0]) + outPut[1];
+    }
+}
